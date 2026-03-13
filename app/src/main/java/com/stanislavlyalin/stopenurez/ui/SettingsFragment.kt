32e925cb39020b6e75d6bf9bfc158e40323de64b
lyalin <lyalin@retail-soft.pro>
2026-03-13 15:53:21 +0300

    Add base permission and navigation

---
+++ app/src/main/java/com/stanislavlyalin/stopenurez/ui/SettingsFragment.kt
@@ -0,0 +1,13 @@
+package com.stanislavlyalin.stopenurez.ui
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import com.stanislavlyalin.stopenurez.R
+
+class SettingsFragment : Fragment(R.layout.fragment_settings) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
---
+++ app/src/main/java/com/stanislavlyalin/stopenurez/ui/UsageFragment.kt
@@ -0,0 +1,13 @@
+package com.stanislavlyalin.stopenurez.ui
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import com.stanislavlyalin.stopenurez.R
+
+class UsageFragment : Fragment(R.layout.fragment_usage) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
